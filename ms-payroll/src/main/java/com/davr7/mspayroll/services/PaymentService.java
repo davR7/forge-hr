206c6ed1278ef5b22e464863e690a0452acf0b9e
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 12:59:17 -0300

    added - handleReadEmployeeById method of the PaymentService

--- ms-payroll/src/main/java/com/davr7/mspayroll/services/PaymentService.java
+++ ms-payroll/src/main/java/com/davr7/mspayroll/services/PaymentService.java
@@ -1,12 +1,20 @@
 package com.davr7.mspayroll.services;
 
+import com.davr7.mspayroll.domain.Employee;
 import com.davr7.mspayroll.domain.Payment;
+import com.davr7.mspayroll.feignclients.EmployeeFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
-    public Payment readEmployeePay(String id, Integer days) {
-        return new Payment("User Mock", 200.0, 30);
+    @Autowired
+    EmployeeFeignClient employeeFeignClient;
+
+    public Payment readEmployeePay(String employeeId, Integer days) {
+        Employee employee = employeeFeignClient.handleReadEmployeeById(employeeId).getBody();
+        if (employee == null) throw new IllegalArgumentException();
+        return new Payment(employee.getName(), employee.getDailyIncome(), days);
     }
 }
