1e0994bb6d8322315d66050df1725fbdb20a7981
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 09:30:22 -0300

    added - readEmployeeById and readAllEmployees methods of the EmployeeResource class

--- ms-employee/src/main/java/com/davr7/msemployee/resources/EmployeeResource.java
+++ ms-employee/src/main/java/com/davr7/msemployee/resources/EmployeeResource.java
@@ -1,9 +1,15 @@
 package com.davr7.msemployee.resources;
 
+import com.davr7.msemployee.domain.Employee;
 import com.davr7.msemployee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="employee")
@@ -11,4 +17,14 @@ public class EmployeeResource {
 
     @Autowired
     EmployeeService employeeServ;
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> handleReadEmployeeById(@PathVariable String id) {
+        Employee obj = employeeServ.readEmployeeById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    @GetMapping
+    public ResponseEntity<List<Employee>> handleReadAllEmployee() {
+        List<Employee> list = employeeServ.readAllEmployees();
+        return ResponseEntity.ok().body(list);
+    }
 }
