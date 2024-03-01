0021f8d36f0cde99ef587a41c7d73dadb0b668d7
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 10:02:46 -0300

    created - first commit for ms-payroll project

---
+++ ms-payroll/src/main/java/com/davr7/mspayroll/MsPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.davr7.mspayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPayrollApplication.class, args);
+	}
+
+}
