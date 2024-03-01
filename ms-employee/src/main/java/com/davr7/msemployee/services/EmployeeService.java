d612d7ebe360b16702aa521808e77c686bea4e7c
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 09:29:34 -0300

    added - readEmployeeById and readAllEmployees methods of the EmployeeService class

--- ms-employee/src/main/java/com/davr7/msemployee/services/EmployeeService.java
+++ ms-employee/src/main/java/com/davr7/msemployee/services/EmployeeService.java
@@ -1,11 +1,25 @@
 package com.davr7.msemployee.services;
 
+import com.davr7.msemployee.domain.Employee;
 import com.davr7.msemployee.repositories.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepo;
+
+    public Employee readEmployeeById(String id) {
+        Optional<Employee> obj = employeeRepo.findById(id);
+        return obj.orElseThrow(() -> new EntityNotFoundException("Employee not found"));
+    }
+
+    public List<Employee> readAllEmployees() {
+        return employeeRepo.findAll();
+    }
 }
