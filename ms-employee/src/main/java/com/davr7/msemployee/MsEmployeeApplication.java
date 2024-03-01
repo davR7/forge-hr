d2937ee0bf92426a9b00cc69c0549a560a49ed7a
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 07:41:49 -0300

    created - first commit for ms-employee project

---
+++ ms-employee/src/main/java/com/davr7/msemployee/MsEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.davr7.msemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmployeeApplication.class, args);
+	}
+
+}
---
+++ ms-employee/src/test/java/com/davr7/msemployee/MsEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davr7.msemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
