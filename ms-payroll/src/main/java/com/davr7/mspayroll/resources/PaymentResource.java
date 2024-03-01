df7b3a4882bd5b7115ba015d6b498d3b3ae27c1b
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 11:03:10 -0300

    created - PaymentResorce class

---
+++ ms-payroll/src/main/java/com/davr7/mspayroll/resources/PaymentResource.java
@@ -0,0 +1,9 @@
+package com.davr7.mspayroll.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("payment")
+public class PaymentResource {
+}
