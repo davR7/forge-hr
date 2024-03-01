fc436204f99833aba17f1a2d9767a20f183feda5
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 11:20:21 -0300

    added - readEmployeePay method of the PaymentResource class (user mock)

--- ms-payroll/src/main/java/com/davr7/mspayroll/services/PaymentService.java
+++ ms-payroll/src/main/java/com/davr7/mspayroll/services/PaymentService.java
@@ -1,7 +1,12 @@
 package com.davr7.mspayroll.services;
 
+import com.davr7.mspayroll.domain.Payment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+
+    public Payment readEmployeePay(String id, Integer days) {
+        return new Payment("User Mock", 200.0, 30);
+    }
 }
