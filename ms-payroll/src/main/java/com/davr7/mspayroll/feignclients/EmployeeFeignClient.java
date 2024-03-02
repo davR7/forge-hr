853b647938a172df7fca9d9f17ea289967f34583
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-01 21:33:29 -0300

    updated - changed name property hr-employee for ms-employee and removed url property, ms-payroll

--- ms-payroll/src/main/java/com/davr7/mspayroll/feignclients/EmployeeFeignClient.java
+++ ms-payroll/src/main/java/com/davr7/mspayroll/feignclients/EmployeeFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name="hr-employee", url = "localhost:8081", path="employee")
+@FeignClient(name="ms-employee", path="employee")
 public interface EmployeeFeignClient {
 
     @GetMapping("/{id}")
