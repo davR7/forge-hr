4379b3213aa837afe407b9d056537047cad4f62e
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-03 21:03:18 -0300

    created - changed EntityNotFoundException to ResourceNotFoundException for ms-employee

--- ms-employee/src/main/java/com/davr7/msemployee/services/EmployeeService.java
+++ ms-employee/src/main/java/com/davr7/msemployee/services/EmployeeService.java
@@ -2,6 +2,7 @@ package com.davr7.msemployee.services;
 
 import com.davr7.msemployee.domain.Employee;
 import com.davr7.msemployee.repositories.EmployeeRepository;
+import com.davr7.msemployee.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class EmployeeService {
 
     public Employee readEmployeeById(String id) {
         Optional<Employee> obj = employeeRepo.findById(id);
-        return obj.orElseThrow(() -> new EntityNotFoundException("Employee not found"));
+        return obj.orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
     }
 
     public List<Employee> readAllEmployees() {
