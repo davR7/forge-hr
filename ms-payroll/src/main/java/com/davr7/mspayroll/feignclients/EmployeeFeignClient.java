4db1caf89ca7f42a76676429b62779ab83616301
Davidson Ratis <davidsonmakarios@gmail.com>
2024-03-05 22:05:41 -0300

    added - configuration property in annotation @FeignClient

--- ms-payroll/src/main/java/com/davr7/mspayroll/feignclients/EmployeeFeignClient.java
+++ ms-payroll/src/main/java/com/davr7/mspayroll/feignclients/EmployeeFeignClient.java
@@ -1,6 +1,7 @@
 package com.davr7.mspayroll.feignclients;
 
 import com.davr7.mspayroll.domain.Employee;
+import com.davr7.mspayroll.feignclients.decoder.EmployeeErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name="ms-employee", path="employee")
+@FeignClient(name="ms-employee", path="employee", configuration = EmployeeErrorDecoder.class)
 public interface EmployeeFeignClient {
 
     @GetMapping("/{id}")
